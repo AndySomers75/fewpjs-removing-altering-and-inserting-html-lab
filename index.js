9811b11f711bf8cb1e94c25bc52b04db54bc5071
Andrew <andrew@pureaquasprings.com>
2022-03-17 20:36:21 +1100

    commit tree d2d2006a144bcd65861d7502604a339561814d10

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const body = document.querySelector("main#main");
+body.remove()
+let newHeader = document.createElement("h1");
+document.querySelector('h1#victory')
+newHeader.id = "victory"
+newHeader.innerHTML='Shaun is the champion' 
+
